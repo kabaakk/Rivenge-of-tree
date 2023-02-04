84c9e0388ef5cfe0f9d0da68c612ef95e0e4f21d
ackrad <ackrad123@gmail.com>
2023-02-04 04:52:54 +0300

    sapling spawns on seed time

--- Assets/Scripts/Seed.cs
+++ Assets/Scripts/Seed.cs
@@ -7,6 +7,11 @@ public class Seed : MonoBehaviour
 {
     public static Seed instance;
     public int seedSpeed = 10;
+    
+    [SerializeField] private float seedAirTimer = 0.5f;
+    
+    private float currentSeedAirTimer = 0f;
+    [SerializeField] private GameObject saplingObject;
 
     private void Awake()
     {
@@ -17,6 +22,14 @@ public class Seed : MonoBehaviour
     private void Update()
     {
         transform.position +=  seedSpeed * Time.deltaTime*transform.forward ;
+        
+        currentSeedAirTimer += Time.deltaTime;
+        if (currentSeedAirTimer >= seedAirTimer)
+        {
+            // instantiate sapling at position on y zero
+            Instantiate(saplingObject, new Vector3(transform.position.x, 0, transform.position.z), Quaternion.identity);
+            Destroy(gameObject);
+        }
     }
 
   
