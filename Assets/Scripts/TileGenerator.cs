0cfe51785e0b295d4c455b6332a2fc0f28924c74
ackrad <ackrad123@gmail.com>
2023-02-04 20:59:30 +0300

    fence system improved

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,14 +14,18 @@ public class TileGenerator : MonoBehaviour
     [SerializeField] private float tileLength = 1f;
 
 
+    private List<Transform> fences = new List<Transform>();
     private void Start()
     {
-        GenerateArea(transform.position, 10, 10);
+        //StartCoroutine(GenerateArea(transform.position, 10, 10));
+        ActionManager.instance.ArenaSurvivalStarted += ArenaSurvivalStarted;
+        ActionManager.instance.ArenaSurvived += ArenaSurvived;
     }
 
-    public void GenerateArea(Vector3 startPos, float xDistance, float yDistance)
+    private IEnumerator GenerateArea(Vector3 startPos, float xDistance, float yDistance)
     {
-        
+        transform.position = PlayerArenaEndShoot.instance.transform.position;
+
         Vector3 leftTopPos =startPos + Vector3.forward* yDistance + Vector3.left * xDistance;
         
         float tileCountX = xDistance*2 / tileLength;
@@ -37,10 +42,12 @@ public class TileGenerator : MonoBehaviour
                 GameObject tile = Instantiate(tileObject, transform.position, Quaternion.identity);
                 tile.transform.SetParent(transform);
                 tile.transform.localPosition = leftTopPos;
-
+                fences.Add(tile.transform);
                 tile.transform.rotation = Quaternion.Euler(rotation);
-                tile.transform.localScale = new Vector3(localScaleValue, localScaleValue, localScaleValue);
+                tile.transform.localScale = Vector3.zero;
+                tile.transform.DOScale(new Vector3(localScaleValue, localScaleValue, localScaleValue),0.5f);
                 leftTopPos += Vector3.right*tileLength   * multiplierVar;
+                yield return null;
             }
            
             rotation += Vector3.up * 90;
@@ -50,11 +57,14 @@ public class TileGenerator : MonoBehaviour
                 GameObject tile = Instantiate(tileObject, transform.position, Quaternion.identity);
                 tile.transform.SetParent(transform);
                 tile.transform.rotation = Quaternion.Euler(rotation);
+                fences.Add(tile.transform);
 
-                tile.transform.localScale = new Vector3(localScaleValue, localScaleValue, localScaleValue);
+                tile.transform.DOScale(new Vector3(localScaleValue, localScaleValue, localScaleValue),0.5f);
                 tile.transform.localPosition = leftTopPos;
 
                 leftTopPos += Vector3.back *tileLength  * multiplierVar;
+                yield return null;
+
             }
             
             rotation += Vector3.up * 90;
@@ -66,5 +76,53 @@ public class TileGenerator : MonoBehaviour
       
      
         
+    }
+
+    private void ScaleAllFences()
+    {
+
+        StartCoroutine(ScaleUpCoroutine());
+    }
+
+    private IEnumerator ScaleUpCoroutine()
+    {
+        transform.position = PlayerArenaEndShoot.instance.transform.position;
+        foreach (var fence in fences)
+        {
+            fence.DOScale(new Vector3(localScaleValue, localScaleValue, localScaleValue),0.5f);
+            yield return null;
+        }
+        
+        
+        
+    }
+     
+
+    private void ScaleAllFencesDown()
+    {
+        
+        
+        foreach (var fence in fences)
+        {
+            fence.DOScale(Vector3.zero,0.5f);
+        }
+    }
+    
+    private void ArenaSurvivalStarted()
+    {
+        if (fences.Count > 0)
+        {
+            ScaleAllFences();
+        }
+        else
+        {
+            StartCoroutine(GenerateArea(transform.position, 14, 9));
+
+        }
+    }
+
+    private void ArenaSurvived()
+    {
+        ScaleAllFencesDown();
     }
 }
